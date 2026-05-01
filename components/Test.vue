450614f63104be1ec70ee0d5cbe8198e0c8e45b8
christina chen <158211764+christinac09@users.noreply.github.com>
2026-05-01 10:33:25 -0400

    body

--- components/Test.vue
+++ components/Test.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="space-y-2">
-    <!-- toolbar -->
-    <div class="flex items-center gap-4">
+    <div class="flex items-center gap-2">
       <select
         v-model="contentType"
         class="bg-gray-800 text-gray-300 px-2 py-1 rounded"
@@ -10,19 +9,21 @@
         <option value="text">Text</option>
         <option value="xml">XML</option>
       </select>
+
       <button
         @click="formatBody"
-        class="bg-gray-700 hover:bg-gray-600 px-2 py-1 text-sm rounded text-gray-200"
+        class="px-2 py-1 bg-gray-700 text-sm rounded hover:bg-gray-600"
       >
         Format
       </button>
-      <div v-if="error" class="text-red-400 text-sm">{{ error }}</div>
+      <div v-if="error" class="text-red-400 text-sm">
+        {{ error }}
+      </div>
     </div>
 
-    <!-- bodyeditor -->
     <div class="relative flex">
       <!-- line #s -->
-      <div class="text-right pr-2 text-gray-500 select-none bg-gray-200 py-2">
+      <div class="text-right pr-2 text-gray-500 select-none bg-gray-900 py-2">
         <div
           v-for="lineNumber in lineCount"
           :key="lineNumber"
@@ -31,34 +32,51 @@
           {{ lineNumber }}
         </div>
       </div>
+
       <textarea
         v-model="body"
+        spellcheck="false"
         placeholder="Enter request body..."
-        class="flex-1 px-2 py-2 rounded-r outline-none resize-none font-mono text-sm leading-5 h-64"
-      ></textarea>
+        class="flex-1 px-2 py-2 bg-gray-800 text-gray-200 rounded-r outline-none resize-none font-mono text-sm leading-5 h-64"
+      />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const error = ref("");
-const contentType = ref<"json" | "text" | "xml">("json");
 const body = ref("");
+const contentType = ref<"json" | "text" | "xml">("json");
+const error = ref("");
+
 const lineCount = computed(() => {
   return body.value.split("\n").length;
 });
 
 function formatBody() {
   error.value = "";
+
   if (contentType.value === "json") {
     try {
       const parsed = JSON.parse(body.value);
-      body.value = JSON.stringify(parsed, null, 2); //the 2 is the indent
+      body.value = JSON.stringify(parsed, null, 2);
     } catch (e) {
-      error.value = "Invalid JSON"; //can only format if in valid JSON
+      error.value = "Invalid JSON";
     }
   }
 }
+
+watch(body, () => {
+  if (contentType.value === "json") {
+    try {
+      JSON.parse(body.value);
+      error.value = "";
+    } catch {
+      error.value = "Invalid JSON";
+    }
+  } else {
+    error.value = "";
+  }
+});
 </script>
 
 <style scoped></style>
